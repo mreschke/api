7eb4591b4f013c603b7f6843b2fdc5d87a573904
Matthew Reschke <mreschke@dynatronsoftware.com>
2016-04-27 16:02:43 -0500

    Updates

--- Http/Controllers/Controller.php
+++ Http/Controllers/Controller.php
@@ -1,11 +1,11 @@
 <?php namespace Mreschke\Api\Http\Controllers;
 
-use Illuminate\Foundation\Bus\DispatchesCommands;
+use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 
-abstract class Controller extends BaseController {
-
-	use DispatchesCommands, ValidatesRequests;
-
+class Controller extends BaseController
+{
+    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
 }
